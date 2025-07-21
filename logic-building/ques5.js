18b4206d7f269bfd9271fdda2a089e6c2a9c5131
Dilshad Ahmad <laddu4591@gmail.com>
2025-07-21 19:12:24 +0530

    Natural no and factorial number problem

---
+++ logic-building/ques5.js
@@ -0,0 +1,21 @@
+// Find the sum of natural number .
+
+let num = 5
+
+let sum = 0;
+for(let i = 1 ;i <= num; i++) {
+    sum = sum + i
+  
+}  console.log(sum)
+
+
+//Find the factorial of a number 
+
+let n = 5 ;
+let fac = 1 ;
+for(let i = 1; i<=n;i++ ) {
+    fac = fac *i
+} console.log(fac)
+
+
+
