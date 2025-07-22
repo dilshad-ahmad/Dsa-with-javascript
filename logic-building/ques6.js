6b1d4a9afee41732893824afe16899a1690ed00e
Dilshad Ahmad <laddu4591@gmail.com>
2025-07-22 12:23:44 +0530

    even no logic

---
+++ logic-building/ques6.js
@@ -0,0 +1,22 @@
+// Find the even number ?
+
+
+//It print all even no .
+ let n = 15
+ for(let i = 1; i <= n ; i++) {
+    if(i%2==0){
+        console.log(i)
+    }
+  
+ }
+
+
+
+//it print only number is even or odd
+// let n = 9 ;
+// if(n%2 == 0) {
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
